eea94a7cdf4303713b238b2dfff76ee74fa86a8a
AnisHakim <hakimanis96@yahoo.fr>
2021-07-11 11:28:20 +0100

    bug fixing add empty data

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
 const MovieCard = ({ movie }) => {
+  return (
+    <div>
+      <h1 style={{ color: 'rgb(64,224,208)' }}>{movie.title}</h1>
+      <img
+        src={movie.image}
+        style={{ width: '300px', height: '200px' }}
+        alt='defaultImg'
+      />
+      <h3 style={{ color: '#00CED1' }}>Description: {movie.description}</h3>
+      <a style={{ color: '#C9C9C9' }} href={movie.posterUrl}>
+        link
+      </a>
+      <h2 style={{ color: '#5F9EA0' }}>Rating: {movie.rating}</h2>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <h1 style={{ color: 'rgb(64,224,208)' }}>{movie.title}</h1>
-            <img src={movie.image} style={{ width: '300px', height: '200px' }} alt="image" />
-            <h3 style={{ color: '#00CED1' }}>Description: {movie.description}</h3>
-            <a style={{ color: '#C9C9C9' }} href={movie.posterUrl}>link</a>
-            <h2 style={{ color: '#5F9EA0' }}>Rating: {movie.rating}</h2>
-        </div>
-    )
-}
-
-export default MovieCard
+export default MovieCard;
--- src/Components/MovieList/MovieList.jsx
+++ src/Components/MovieList/MovieList.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import MovieCard from '../MovieCard'
-import './List.css'
+import React from 'react';
+import MovieCard from '../MovieCard';
+import './List.css';
 const MovieList = ({ movies }) => {
-
-    return (
-        <div className="list">
-            {
-                movies.map((movie, index) => <div className="movie"><MovieCard key={index} movie={movie}
-                /></div>)
-            }
+  return (
+    <div className='list'>
+      {movies.map((movie, index) => (
+        <div className='movie' key={index}>
+          <MovieCard movie={movie} />
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default MovieList
+export default MovieList;
